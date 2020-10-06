d6d6b7453c7778795c14ab773adbeb0ff45124ec
AgusK <agustin.karkocki@gmail.com>
2020-10-06 12:19:15 -0300

    New file template for Contact Page

---
+++ template-contacto.php
@@ -0,0 +1,14 @@
+<?php /* Template Name: Historia */ ?>
+<?php get_header(); ?>
+
+<section class="py-6">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_template_part('parts/donar'); ?>
+<?php get_footer(); ?>
